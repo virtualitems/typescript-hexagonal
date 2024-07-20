ac15eb4118fc3cd7ade43c27e27c18a2a3003e2d
Alejandro CR <contacto@alejandrocr.co>
2024-07-20 14:45:14 -0500

    create Handle.ts class file

---
+++ source/shared/application/events/Handler.ts
@@ -0,0 +1,54 @@
+// Libraries
+
+// Same Shared Module Layer
+
+// Lower Shared Module Layers
+
+// Other Modules
+
+// Same Layer
+
+// Lower Layers
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+ * @description 
+ */
+export default abstract class Handler
+{
+
+  // public ATTRIBUTES
+
+  // protected ATTRIBUTES
+
+  // private ATTRIBUTES
+
+  // public static ATTRIBUTES
+
+  // protected static ATTRIBUTES
+
+  // private static ATTRIBUTES
+
+  // Constructor, Getters, Setters
+
+  // public METHODS
+
+  public abstract handle(payload?: Record<string, unknown>): void;
+
+  // protected METHODS
+
+  // private METHODS
+
+  // public static METHODS
+
+  // protected static METHODS
+
+  // private static METHODS
+
+} //:: class
