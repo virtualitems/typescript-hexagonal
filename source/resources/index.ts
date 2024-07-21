96039411da1b8c26b4a8c8d452f8083c63e2c825
Alejandro CR <contacto@alejandrocr.co>
2024-07-21 01:31:21 -0500

    create index.ts exports file for resources

---
+++ source/resources/index.ts
@@ -0,0 +1,20 @@
+// Adapters layer
+
+// Application layer
+
+import InMemoryResourcesRepository from './application/InMemoryResourcesRepository';
+import ResourcesService from './application/ResourcesService';
+
+// Domain layer
+
+// Constants
+
+// Functions
+
+// Exports
+
+export default
+{
+  ResourcesService,
+  InMemoryResourcesRepository,
+};
