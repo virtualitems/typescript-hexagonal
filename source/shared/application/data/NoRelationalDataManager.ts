fd160031e788ca4e748a75a97abb16b86c7ddd30
Alejandro CR <contacto@alejandrocr.co>
2024-07-22 22:51:43 -0500

    add new data manager templates

---
+++ source/shared/application/data/NoRelationalDataManager.ts
@@ -0,0 +1,46 @@
+// Libraries
+
+// Same Shared Module Layer
+
+// Lower Shared Module Layers
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+ * @description 
+ */
+export default abstract class NoRelationalDataManager
+{
+
+  // public ATTRIBUTES
+
+  // protected ATTRIBUTES
+
+  // private ATTRIBUTES
+
+  // public static ATTRIBUTES
+
+  // protected static ATTRIBUTES
+
+  // private static ATTRIBUTES
+
+  // Constructor, Getters, Setters
+
+  // public METHODS
+
+  // protected METHODS
+
+  // private METHODS
+
+  // public static METHODS
+
+  // protected static METHODS
+
+  // private static METHODS
+
+} //:: class
