ad7674b130c19bbd3f35ceec42d9c11603bb6e33
Alejandro CR <contacto@alejandrocr.co>
2024-08-22 07:27:41 -0500

    update users/index.ts

--- source/context/users/index.ts
+++ source/context/users/index.ts
@@ -1,15 +1,15 @@
 // Adapters layer
 
-export { default as UsersArrayDataManager } from './adapters/UsersArrayDataManager';
+export { default as UsersArrayDataManager } from './adapters/UsersArrayDataManager.js';
 
 // Application layer
 
-export { default as CreateUsersUseCase } from './application/CreateUsersUseCase';
-export { default as FindUsersUseCase } from './application/FindUsersUseCase';
-export { default as UsersRepository } from './application/UsersRepository';
+export { default as CreateUsersUseCase } from './application/CreateUsersUseCase.js';
+export { default as FindUsersUseCase } from './application/FindUsersUseCase.js';
+export { default as UsersRepository } from './application/UsersRepository.js';
 
 // Domain layer
 
-export { default as UsersService } from './domain/UsersService';
+export { default as UsersService } from './domain/UsersService.js';
 
 // Constants
